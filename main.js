bc442f82b991e5980d0c18252d2a8b5514f10aa9
ridasersif <ridasersif1@gmail.com>
2024-11-01 16:15:38 +0100

    vertion 2

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let date=document.getElementById('date');
 let statu=document.getElementById('statu');
 let description=document.getElementById('description');
 let Submit=document.getElementById('Submit');
+let Submit_none=document.getElementById('Submit_none');
 
 
 //ajoute tache
@@ -13,23 +14,34 @@ if(localStorage.SaveTach!=null){
 }else{
     datatache=[];
 }
-
 Submit.onclick=function(){
     let NauveauTache={
         nom:nom.value,
         date:date.value,
         description:description.value,
         statu:statu.value,
+    
     }
     datatache.unshift(NauveauTache)
     localStorage.setItem('SaveTach',JSON.stringify(datatache))
-    clearData()
+    resetData()
     afficheData()
+    ajaute()
+
+}
+function ajaute(){
+    if(nom.value !='' && description.value !='' &&  date.value !='' && statu.value !='default'){
+         Submit.style.display="block"
+         Submit_none.style.display="none"
+    }else{
+        Submit.style.display="none"
+         Submit_none.style.display="block"
+    }
 }
 // clear inputs
-function clearData(){
+function resetData(){
     nom.value = '';
-    statu.value = '';
+    statu.value = 'default';
     date.value = '';
     description.value = '';
 }
@@ -37,28 +49,49 @@ function clearData(){
 function afficheData(){
     let table = '';
     for(let i=0; i<datatache.length ; i++){
-        table +=`
-        <tr>
-                            <td>${datatache[i].nom}</td>
-                            <td>${datatache[i].description}</td>
-                            <td>${datatache[i].statu}r</td>
-                            <td>${datatache[i].date}</td>
-                            <td><button onclick="ModifierData(${i}) " type="button" class="btn btn-primary">Modifier</button></td>
-                            <td><button onclick="SupprimerData(${i})" type="button" class="btn btn-danger">Supprimer</button></td> 
-                                    
-         </tr>
+        table += `
+            <div class="bahom">
+                    <div class="Card">
+                    <h5>Title :</h5>
+                    <p> ${datatache[i].nom}</p>
+                    </div>
+
+                    <div class="Card Card-description">
+                    <h5>Description :</h5>
+                    <p>${datatache[i].description}</p>
+                    </div>
+<div class="statu-date">
+                    <div class="Card">
+                    <h5>Statu :</h5>
+                    <p> ${datatache[i].statu}</p>
+                    </div>
+
+                    <div class="Card">
+                    <h5>date :</h5>
+                    <p> ${datatache[i].date}</p>
+                    </div>
+</div>
+                    <hr>
+
+                    <div class=" button-card">
+                         <button onclick="ModifierData(${i}) " type="button" class="btn btn-link"><i class="fa-regular fa-pen-to-square text-primary"></i></button>
+                         <button onclick="SupprimerData(${i})" type="button" class="btn btn-link"><i class="fa-regular fa-trash-can text-danger"></i></button>
+                         
+                         <hr>            
+                    </div>
+            </div>
                 `
                 }
- document.getElementById('tbody').innerHTML=table;
+ document.getElementById('element').innerHTML=table;
 }
 afficheData();
 //Supprimer une tache.
 function SupprimerData(i){
    datatache.splice(i,1);
    localStorage.SaveTach=JSON.stringify(datatache);
-   console.log(i)
    afficheData();
 }
+
 
 
 
@@ -69,7 +102,7 @@ function ModifierData(i){
     statu.value=datatache[i].statu;
     date.value=datatache[i].date;
     SupprimerData(i)
-    
+   
 }
 
 
